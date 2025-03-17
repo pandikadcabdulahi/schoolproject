8c6c98d8591ec28414f897c579f5a8c161c2f57a
pandikadcabdulahi <pandikadcabdulahi@gmail.com>
2025-03-17 12:11:09 +0000

    Add 11.php

---
+++ src/11.php
@@ -0,0 +1,10 @@
+
+<?php
+$name = 'Jane Doe';
+$age = 30;
+if ($age >= 18) {
+    echo "Welcome, $name! You are eligible to vote.";
+} else {
+    echo "Sorry, $name. You are not eligible to vote yet.";
+}
+?>
