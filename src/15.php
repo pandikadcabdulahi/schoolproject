aca0e65f8000f5923fef63985069917b447ffc0c
pandikadcabdulahi <pandikadcabdulahi@gmail.com>
2025-03-21 08:19:42 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,14 @@
+<?php
+// Sample PHP code to demonstrate variable assignment and basic operations
+
+$number1 = 5;
+$number2 = 3;
+
+// Perform arithmetic operation
+$result = $number1 + $number2;
+echo "The sum of $number1 and $number2 is: $result";
+
+// Print a message with some random text
+$text = "Hello, this is just a simple PHP code for demonstration purposes.";
+echo $text;
+?>
