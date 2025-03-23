f4ee7fc749defee6c75ae68f2617453c0c4fcb90
pandikadcabdulahi <pandikadcabdulahi@gmail.com>
2025-03-23 19:52:32 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,13 @@
+<?php
+// Simple school project for classroom
+
+function addNumbers($a, $b) {
+    return $a + $b;
+}
+
+$a = 5;
+$b = 3;
+
+$sum = addNumbers($a, $b);
+echo "The sum is: $sum";
+?>
