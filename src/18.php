4989ebb4d2e751c70ddb96df2f17d0e712ca49a3
pandikadcabdulahi <pandikadcabdulahi@gmail.com>
2025-03-24 00:38:48 +0000

    Add 18.php

---
+++ src/18.php
@@ -0,0 +1,8 @@
+<?php
+// This PHP file is part of Simple School Project.
+// It's designed to be executed by a web server using a specific configuration script.
+// This example will not generate any output.
+
+// Your application code goes here
+
+?>
