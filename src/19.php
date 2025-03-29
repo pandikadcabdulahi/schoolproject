1488600bed168c3f74be20d7044ec2a33b383156
pandikadcabdulahi <pandikadcabdulahi@gmail.com>
2025-03-29 18:59:19 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,16 @@
+<?php
+// Code to be executed on page load
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple School Project</title>
+</head>
+<body>
+    <?php
+        // Your code here
+        echo "Hello, World!";
+    ?>
+</body>
+</html>
